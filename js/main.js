b6ab7fe2641135e60102335fe5597fb919c95218
Sergio Barrera <sdbeng@gmail.com>
2017-05-18 16:58:56 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  //itest
+  
+
+  // $('div.button').on('click', function() {
+  //   $(this).fadeOut(1000);
+  // });
+
+  //start: title button functionality
+  var title = $('#title-input').val();
+  console.log('title is: ',title);
+  $('.list-title');
+
+  //start: item button functionality
+  var itemText = $('#item-input').val();
+
+})
